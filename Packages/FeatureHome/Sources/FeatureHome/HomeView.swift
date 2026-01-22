0462062576fb13ab2549d51e943a57b886d0fd7c
Uğur hamzaoğlu <ugur.hmz52@gmail.com>
2026-01-22 15:01:19 +0300

    All package swift tool version down grade 5.9 for Strict policy

---
+++ Packages/FeatureHome/Sources/FeatureHome/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  FeatureHome
+//
+//  Created by rico on 22.01.2026.
+//
+
+import Foundation
+import SwiftUI
+
+
+public struct HomeView: View {
+    public var body: some View {
+        Text("A")
+    }
+}
+
+#Preview {
+    HomeView()
+}
